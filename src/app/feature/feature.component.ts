ff0cba82afdbf986d274acf38ebb37d2318fb1b5
WildPastry <mykdsn@gmail.com>
2021-11-06 21:22:11 +1300

    Added background image

--- src/app/feature/feature.component.ts
+++ src/app/feature/feature.component.ts
@@ -6,8 +6,16 @@ import { ContentfulService } from "../contentful.service";
   templateUrl: "./feature.component.html",
   styleUrls: ["./feature.component.scss"],
 })
+
 export class FeatureComponent implements OnInit {
+  styleObject(): Object {
+    return { height: "500px" };
+  }
+  
   images$ = this.contentfulService.getImages();
-  constructor(private contentfulService: ContentfulService) {}
+
+  constructor(private contentfulService: ContentfulService) {
+    console.log(this.images$, this.styleObject)
+  }
   ngOnInit() {}
 }
