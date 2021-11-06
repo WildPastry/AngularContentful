c2f7ded7f9f6a614508583acaa86bad21e33943e
WildPastry <mykdsn@gmail.com>
2021-11-07 08:27:30 +1300

    Changing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FeatureComponent } from './feature/feature.component';
 import { PostsPageComponent } from './posts-page/posts-page.component';
 import { PostDetailsComponent } from './post-details/post-details.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: FeatureComponent
+  },
   {
     path: 'posts',
     component: PostsPageComponent
