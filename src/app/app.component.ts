85bb5fe16ccfb2f912afc997d53b5b25ef74a3fd
WildPastry <mykdsn@gmail.com>
2021-11-15 19:21:10 +1300

    Fixed routes, added child element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,105 +1,34 @@
-import { Component, ElementRef, HostListener, OnInit, ViewChild } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { ContentfulService } from "./contentful.service";
-import { Router, NavigationEnd, NavigationStart } from '@angular/router';
-import { ViewportScroller } from "@angular/common";
+import { Router, NavigationEnd } from "@angular/router";
+// component declarations
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
+// export component
 export class AppComponent implements OnInit {
+  // child element
+  @ViewChild("scrollAnchor", { static: false }) scrollAnchor: ElementRef;
 
-  // [x: string]: any;
-
-  @ViewChild('scrollAnchor', {static: false}) topScroll: ElementRef;
-
-  // onNavigate(event): any {
-  //   this.topScroll.nativeElement.scrollIntoView({ behavior: 'smooth' });
-  // }
-  
   // contentfulService
   images$ = this.contentfulService.getImages();
 
-  // pageYoffset: number;
-  // router: any;
-
-  constructor(private contentfulService: ContentfulService, public router: Router) {
-
+  // constructor - add contentfulService and router
+  constructor(
+    private contentfulService: ContentfulService,
+    public router: Router
+  ) {
+    // scroll to child element on route change
     this.router.events.subscribe((ev) => {
-      if (ev instanceof NavigationEnd) { 
-        // console.log('NavigationEnd');
-        // window.scrollTo(0, 0);
-
-        // document.getElementById("scrollAnchor").scrollIntoView()
-        this.topScroll.nativeElement.scrollIntoView()
-        // setTimeout(function(){ window.scrollTo(0, 0) }, 3000);
-
-        // setTimeout(function(){ document.getElementById("scrollAnchor").scrollIntoView() }, 1000);
-
-        // console.log(this.router.config);
-        /* Your code goes here on every router change */}
+      if (ev instanceof NavigationEnd) {
+        this.scrollAnchor.nativeElement.scrollIntoView();
+      }
     });
   }
 
-//   constructor(public router: Router) {
-//     this.changeOfRoutes();
-
-//     this.router.events
-//         .filter(event => (event instanceof NavigationEnd))
-//             .subscribe((routeData: any) => {
-//                 if(routeData.urlAfterRedirects === '/') {
-//                     this.showAfterLogin = true;
-//                 }
-//             });
-// }
-
-  // scroll top top fucntion
-  // runs every page route
-  // onActivate(event) {
-  //   console.log('SCROLL TO TOP');
-  //   window.scrollTo(0, 0);
-  // }
-
-  // constructor(private router: Router) {
-  //   this.router.events.subscribe((ev) => {
-  //     if (ev instanceof NavigationEnd) { /* Your code goes here on every router change */}
-  //   });
-
-//   app.run(['$transitions', function ($transitions) {
-//     $transitions.onSuccess({}, function () {
-//         document.body.scrollTop = document.documentElement.scrollTop = 0;
-//     })
-// }]);
-
-//   $rootScope.$on('$stateChangeSuccess', function() {
-//     document.body.scrollTop = document.documentElement.scrollTop = 0;
-//  });
- 
-//   @HostListener('window:scroll', ['$event']) onScroll(event){
-//     this.pageYoffset = window.pageYOffset;
-//  }
-//  scrollToTop(){
-//   this.scroll.scrollToPosition([0,0]);
-// }
-//   onActivate(event) {
-//     window.scroll(0,0);
-//     //or document.body.scrollTop = 0;
-//     //or document.querySelector('body').scrollTo(0,0)
-// }
-ngOnInit(){
-  // this.router.events.subscribe(event =>{
-  //    if (event instanceof NavigationStart){
-  //       console.log(event.url)
-  //       this.routerChangeMethod(event.url);
-  //    }
-  // })
-}
-//   ngOnInit() {   
-// //        this.router.events.subscribe((event) => {
-// //     if (!(event instanceof NavigationEnd)) {
-// //         return;
-// //     }
-// //     window.scrollTo(0, 0)
-// // });
-// }
+  ngOnInit() {
+    console.log("ngOnInit AppComponent");
+  }
 }
